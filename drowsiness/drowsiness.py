6f8f22c95e05bb5b1977cf852d2c68f61b9d2cf1
Ranganath0108 <ranganathswamy.ys@gmail.com>
2022-05-04 18:28:48 +0530

    lip distance function is added

--- drowsiness/drowsiness.py
+++ drowsiness/drowsiness.py
@@ -26,3 +26,36 @@ def eye_aspect_ratio(eye):
     E_A_R = (A + B) / (2.0 * C)
 
     return E_A_R
+
+
+# Final eye Aspect ratio caluclation for both eyes
+
+def final_EAR(shape):
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+    leftEye = shape[lStart:lEnd]
+    rightEye = shape[rStart:rEnd]
+
+    leftEAR = eye_aspect_ratio(leftEye)
+    rightEAR = eye_aspect_ratio(rightEye)
+
+    ear = (leftEAR + rightEAR) / 2.0
+    return (ear, leftEye, rightEye)
+
+
+# To Caluclate lip distance
+
+
+def lip_distance(shape):
+    top_lip = shape[50:53]
+    top_lip = np.concatenate((top_lip, shape[61:64]))
+
+    low_lip = shape[56:59]
+    low_lip = np.concatenate((low_lip, shape[65:68]))
+
+    top_mean = np.mean(top_lip, axis=0)
+    low_mean = np.mean(low_lip, axis=0)
+
+    distance = abs(top_mean[1] - low_mean[1])
+    return distance
